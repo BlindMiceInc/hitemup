adc41b5c88742260f62e390f9c04b25a7a7eb6e8
Qadir Haqq <haqq.q173@gmail.com>
2020-06-08 13:03:00 -0700

    Finished basic front end flow, added bootstrap 4 styles

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,5 +1,15 @@
-import React from "react"
+import React from 'react';
+import MailInForm from './components/mailInForm';
+import 'bootstrap/dist/css/bootstrap.css';
+
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div className="jumbotron">
+      <h1>Mail Activism Website</h1>
+      <MailInForm></MailInForm>
+    </div>
+  );
 }
